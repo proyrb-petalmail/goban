419fd61da7e2b8ce85a2b4f401fdb9acb0faad7a
Scray <2487298826@qq.com>
2024-10-01 11:55:24 +0800

    v0.4

---
+++ source/printer/printer.c
@@ -0,0 +1,9 @@
+#include "debug.h"
+#include "gameboard.h"
+
+void print_gameboard(void)
+{
+    Debug(Message_Level, "width:%d \theight:%d\n", gameboard_get_width(), gameboard_get_height());
+
+    //
+}
