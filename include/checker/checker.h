fb0e399e9f9151ca897925be1168c3243dfe9e68
Scray <2487298826@qq.com>
2024-10-02 20:39:32 +0800

    update

--- include/checker/checker.h
+++ include/checker/checker.h
@@ -3,6 +3,12 @@
 #include "gameboard_type.h"
 
 /**
- *
+ * @brief Check if the game has been won.
+ * @param unit the coord of unit.
+ * @return return 0 if Achieving Victory
+ *         return -1 if Invalid piece
+ * @version 1.0
+ * @date 2024/10/1
+ * @author SCR
  */
 char checker_determine(gameboard_unit_coord *const coord);
--- source/checker/checker.c
+++ source/checker/checker.c
@@ -12,6 +12,7 @@ char checker_determine(gameboard_unit_coord *const coord)
     /* '\' */
     int count = 1;
     gameboard_unit_coord *const new_coord = gameboard_get_coord(0, 0);
+    // 0 = gameboard_set_coord( coord, , x_of_width)
     for (unsigned char index = 1; index < 5; ++index)
     {
         if (gameboard_get_stone_at_coord(new_coord) == base_stone)
@@ -32,6 +33,29 @@ char checker_determine(gameboard_unit_coord *const coord)
             break;
     }
 
+    gameboard_recycle_coord(coord);
+    return -2;
+
+    /* '/' */
+    for (unsigned char index = 1; index < 5; ++index)
+    {
+        if (gameboard_get_stone_at_coord(new_coord) == base_stone)
+        {
+            ++count;
+        }
+        else
+            break;
+    }
+    for (unsigned char index = 1; index < 5; ++index)
+    {
+        gameboard_unit_coord *const new_coord = {coord->y_of_height + index, coord->x_of_width - index};
+        if (gameboard_get_stone_at_coord(new_coord) == base_stone)
+            ++count;
+        else
+            break;
+        if (count > 4)
+            return 0;
+    }
     gameboard_recycle_coord(coord);
     return -2;
 }
